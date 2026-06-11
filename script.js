0e9cfe2cb36e761f23c6720a5990f1f59365dc79
Ilya <ilya.batskell@bk.ru>
2026-06-11 04:09:04 +0300

    add map with banner, styling and add contacts footer

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const sliderRange = document.getElementById("sliderRange");
 const afterImage = document.getElementById("afterImg");
-const percentLabel = document.getElementById("percentLabel");
 
 function updateComparison(value) {
   let percent = parseFloat(value);
@@ -14,3 +13,33 @@ function updateComparison(value) {
 sliderRange.addEventListener("input", function (e) {
   updateComparison(e.target.value);
 });
+
+ymaps.ready(init);
+
+function init() {
+  var myMap = new ymaps.Map(
+    "map",
+    {
+      center: [59.938631, 30.323037],
+      zoom: 19,
+      controls: [],
+    },
+    {
+      suppressMapOpenBlock: true,
+    },
+  );
+  var myPlacemark = new ymaps.Placemark(
+    [59.938631, 30.323037],
+    {
+      hintContent: "Cat Energy", 
+      balloonContent: "Санкт-Петербург, Большая Конюшенная улица, 19/8",
+    },
+    {
+      iconLayout: "default#image",
+      iconImageHref: "./assets/MapPin.svg", 
+      iconImageSize: [80, 80], 
+      iconImageOffset: [-20, -50], 
+    },
+  );
+  myMap.geoObjects.add(myPlacemark);
+}
